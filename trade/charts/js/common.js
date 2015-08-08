98b78190f962d9c1bb2a086630235d484bc51183
greenteadigital <ben@greenteadigital.com>
2015-08-08 14:43:13 -0400

    move common js into its own file

---
+++ trade/charts/js/common.js
@@ -0,0 +1,10 @@
+var log = console.log,
+dataServer = 'http://localhost',
+w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+cwidth = w.innerWidth || e.clientWidth || g.clientWidth,
+cheight = w.innerHeight|| e.clientHeight|| g.clientHeight,
+margin = 35,
+zoomMult = 1;
