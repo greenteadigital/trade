8a58c69718429c140d9d6a0affa19ca2bde4efff
greenteadigital <ben@greenteadigital.com>
2015-08-09 23:15:38 -0400

    simply code that gets the location hash string

--- trade/code/symbols.py
+++ trade/code/symbols.py
@@ -1,6 +1,20 @@
 import os, json
 dir_ = r".\data\csv"
 
-def getSyms(*args):
-	symbols = map(lambda f: f.split('.')[0].lstrip('_'), os.listdir(dir_))
-	return json.dumps(symbols)
+def numLines(fname):
+    with open(fname) as f:
+        for count, line in enumerate(f, 1):
+            pass
+    return count
+
+def getSyms(params):
+	order = params['order'][0]
+	
+	if order == 'aplha':
+		symbols = map(lambda f: f.split('.')[0].lstrip('_'), os.listdir(dir_))
+		return json.dumps(symbols)
+		
+	elif order == 'biggest':
+		hfile = r".\data\json\history.json"
+		hist = json.load(open(hfile,'rb'))
+		return json.dumps(map(lambda map: map.keys()[0] , sorted(hist, key=lambda x: x.values()[0], reverse=True)))
