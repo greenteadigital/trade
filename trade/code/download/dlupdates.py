da594a31106710fe42e7229e27298996ed33efaf
greenteadigital <ben@greenteadigital.com>
2015-08-30 22:31:47 -0400

    Continuing to gather the symbols to update

--- trade/code/download/dlupdates.py
+++ trade/code/download/dlupdates.py
@@ -5,11 +5,31 @@ import csv
 import urllib2
 from cStringIO import StringIO as strio
 import const
+from const import pjoin
+from fnmatch import fnmatch
+import sys
 
 # build URLs with symbols and intended date range
 # make requests to yahoo in round-robin fashion
 def downloadEodData():
-	txt_syms = 
+	txts = filter(lambda fn: fnmatch(fn, '*listed_*.txt'), os.listdir(const.SYM_DIR))
+	
+	nsdl = filter(lambda fn: fnmatch(fn, 'nasdaqlisted_*.txt'), txts)[0]
+# 	print nsdl
+	allnsd = list(csv.DictReader(open(pjoin(const.SYM_DIR, nsdl), 'rb'), delimiter="|"))
+	nsdsyms = filter(lambda s: s.isalpha(), map(lambda d: d['Symbol'], allnsd))
+	
+	othl = filter(lambda fn: fnmatch(fn, 'otherlisted_*.txt'), txts)[0]
+	allother = list(csv.DictReader(open(pjoin(const.SYM_DIR, othl), 'rb'), delimiter="|"))
+	othsyms = filter(lambda s: s.isalpha(), map(lambda d: d['ACT Symbol'], allother))
+	
+	txt_syms = nsdsyms + othsyms
+	txt_syms.sort()
+	for s in txt_syms:
+		print s
+	sys.exit()
+	
+# 	txt_syms = 
 	fs_syms = map(lambda n: n.split('.')[0], os.listdir(const.RAW_DIR))
 	syms = filter(lambda i: i.isalpha(), map(lambda i: i.lstrip('_'), list(set(txt_syms) - set(fs_syms))))
 	for symbol in syms:
